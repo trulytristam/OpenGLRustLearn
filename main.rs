fea79e10b50dabc9a5fbc3e50a61674630a2bf91
trulytristam <49181012+trulytristam@users.noreply.github.com>
2023-01-05 05:51:45 -0500

    first commit

---
+++ main.rs
@@ -0,0 +1,41 @@
+
+
+use glutin::event_loop::*;
+use glutin::*;
+use glutin::window::*;
+use glutin::event::*;
+
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().with_title("Learn OpenGL with Rust");
+
+    let gl_context = ContextBuilder::new()
+    .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
+    .build_windowed(window, &event_loop)
+    .expect("Cannot create windowed context");
+
+    let gl_context = unsafe {
+    gl_context
+        .make_current()
+        .expect("Failed to make context current")
+    };
+
+   gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+
+    event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+        Event::LoopDestroyed => (),
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            _ => (),
+        },
+        _ => (),
+    }
+    });
+
+  
+
+}
